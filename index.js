7822b625a59a1330606a519283774e05ae4c457e
Roderick Shoolbraid <rdxenv@gmail.com>
2023-10-22 13:33:22 -0700

    function update

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 // INDEX.JS for W5 "Cats as a service"
-//MORE TESTS 
+
+//  request logic ..
+
+const fetchBreedDescription = (breedName, callback) => {
+  request(BASE_URL + breedName, (error, response, body) => {
+  // Check for request errors
+    if (error) {
+      callback(`Failed to fetch breed details: ${error}`, null);
+      return;
+    }
+
+    const data = JSON.parse(body);
+
+    // Check if the breed is not found
+    if (data.length === 0) {
+      callback(`Could not find breed: ${breedName}`, null);
+      return;
+    }
+
+    // Extract and return the breed description
+    const breed = data[0];
+    callback(null, breed.description);
+  });
+};
